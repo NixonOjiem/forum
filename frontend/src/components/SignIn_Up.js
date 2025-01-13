542b850cdf339190932f05970efb04ad1ab530d1
arkstudiodevelopers <nicksonogm@gmail.com>
2025-01-13 15:32:45 +0300

    added a sign in UI

--- frontend/src/components/SignIn_Up.js
+++ frontend/src/components/SignIn_Up.js
@@ -9,8 +9,9 @@ const SignIn_Up=()=> {
   const [first_name, setFirstName]= useState('')
   const [last_name, setLastName]= useState('')
   const [username, setUserName]= useState('')
+  const [showSignup, setShowSignup] = useState(false)
 
-  const handleSubmit= async (e)=> {
+  const handleSignup= async (e)=> {
     e.preventDefault()
     try{
       const response = await axios.post('http://localhost:3001/signup', {first_name, last_name, email, username, password })
@@ -25,14 +26,23 @@ const SignIn_Up=()=> {
     }
   }
 
+  const handleLogin = async (e)=> {
+    e.preventDefault()
+    try{
+      const response = await axios.post('http://localhost:3001/login', {email, password} )
+    }
+    catch(error){}
+  }
+
   return (
-    <div className='Signin-Container'>
+    <div>{ showSignup? (
+      <div className='auth-Container'>
 
         <div className='banner'>
           <img src='/images/Login_question_with_background.jpg' alt='login picture with students' className='banner-pic'/>
         </div>
 
-        <form onSubmit={handleSubmit} className='Signup-form'>
+        <form onSubmit={handleSignup} className='auth-form'>
           <h1 className='heading-registration'>Registration Form</h1>
           <label className='signup-label'>First Name:</label>
           <input type="text" value={first_name} onChange={(e)=>setFirstName(e.target.value)} /> 
@@ -50,7 +60,28 @@ const SignIn_Up=()=> {
           <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)} />
           <br />
           <button className='btn'type='submit'>Register</button>
+          <button onClick={() => setShowSignup(false)}>Back to Sign In</button>
         </form>
+      </div>):(
+
+        <div className='auth-container'>
+          <div className='banner'>
+          <img src='/images/Login_question_with_background.jpg' alt='login picture with students' className='banner-pic'/>
+        </div>
+          <form onSubmit={handleLogin} className='auth-form'>
+            <h1>Sign in</h1>
+            <label>Username</label>
+            <input type='text' value={username} onChange={(e)=>setUserName(e.target.value)} />
+            <br />
+            <label>Password</label>
+            <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)} />
+            <br />
+            <button className='btn'type='submit'>Login</button>
+            <button className='btn' onClick={()=>setShowSignup(true)}>Sign Up</button>
+          </form>
+        </div>
+      )
+    }
     </div>
   )
 }
