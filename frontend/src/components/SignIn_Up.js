aea9466077ca38ec1ee9688f133df9c60425e538
arkstudiodevelopers <nicksonogm@gmail.com>
2025-01-13 14:13:45 +0300

    Started working on the registration form

--- frontend/src/components/SignIn_Up.js
+++ frontend/src/components/SignIn_Up.js
@@ -1,6 +1,7 @@
 import React, {useState}from 'react'
 import axios from 'axios'
 
+
 const SignIn_Up=()=> {
   
   const [email, setEmail]= useState('')
@@ -26,21 +27,26 @@ const SignIn_Up=()=> {
 
   return (
     <div className='Signin-Container'>
-        <form onSubmit={handleSubmit}>
-          <h1>Registration Form</h1>
-          <label>First Name:</label>
+
+        <div className='banner'>
+          <img src='/images/Login_question_with_background.jpg' alt='login picture with students' className='banner-pic'/>
+        </div>
+
+        <form onSubmit={handleSubmit} className='Signup-form'>
+          <h1 className='heading-registration'>Registration Form</h1>
+          <label className='signup-label'>First Name:</label>
           <input type="text" value={first_name} onChange={(e)=>setFirstName(e.target.value)} /> 
           <br />
-          <label>Last Name:</label>
+          <label className='signup-label'>Last Name:</label>
           <input type='text' value={last_name} onChange={(e)=>setLastName(e.target.value)} />
           <br />
-          <label>Email:</label>
+          <label className='signup-label'>Email:</label>
           <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} />
           <br />
-          <label>Username:</label>
+          <label className='signup-label'>Username:</label>
           <input type='text' value={username} onChange={(e)=>setUserName(e.target.value)} />
           <br />
-          <label>Password:</label>
+          <label className='signup-label'>Password:</label>
           <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)} />
           <br />
           <button className='btn'type='submit'>Register</button>
