9467e47c806fbe8a6f3d64b394f4fd98ebba8436
arkstudiodevelopers <nicksonogm@gmail.com>
2025-01-16 21:26:33 +0300

    test

--- frontend/src/components/Forums.js
+++ frontend/src/components/Forums.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './Header'
+import SideBar from './SideBar'
+import QuestionList from './QuestionList'
 
 function Forums() {
   return (
     <div className='forum-container'>
         <Header />
+        <SideBar />
+        <QuestionList />
     </div>
   )
 }
