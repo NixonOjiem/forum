0c5a56816acab6fe103d221bb5f5237f3f404c67
arkstudiodevelopers <nicksonogm@gmail.com>
2025-02-04 10:14:49 +0300

    still working on the ask a question component.

--- frontend/src/components/AskQuestion.js
+++ frontend/src/components/AskQuestion.js
@@ -2,11 +2,21 @@ import React from 'react'
 
 function AskQuestion() {
   return (
-    <div>
+    <div className='Ask-Question'>
         <h1>Ask a Question</h1>
-        <label>Title</label> <br />
-        <input type='text' />
-        
+        <label className='Labels'>Title</label>
+        <input type='text' className='Title-Textbox' placeholder='Add your question title here'/>
+        <label className='Labels'>Body</label>
+        <input type='text' className='Body-Textbox' placeholder='Ask your question here' />
+        <label className='Labels'>Tags</label>
+        <select name='Tags' className='Labels'>
+            <option value={"Tag1"}>Tag1</option>
+            <option value={"Tag2"}>Tag2</option>
+            <option value={"Tag3"}>Tag3</option>
+            <option value={"Tag4"}>Tag4</option>
+        </select>
+        {/* <input type='select' className='Tags-Textbox'/> */}
+        <button className='button'>Post Your Question</button>
     </div>
   )
 }
