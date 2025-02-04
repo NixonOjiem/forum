7172faa07ab8e1151ec055356a1586b2303aa7fd
arkstudiodevelopers <nicksonogm@gmail.com>
2025-02-04 10:35:14 +0300

    About to start working on the editing capabilities of the question text box

--- frontend/src/components/AskQuestion.js
+++ frontend/src/components/AskQuestion.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AskQuestion() {
+    const [question, setQuestion]= useState('');
+    const [title, setTitle]= useState('');
+    const [tag, setTag]= useState('');
+    
   return (
     <div className='Ask-Question'>
         <h1>Ask a Question</h1>
         <label className='Labels'>Title</label>
-        <input type='text' className='Title-Textbox' placeholder='Add your question title here'/>
+        <input type='text' className='Title-Textbox' placeholder='Add your question title here' value={title}/>
         <label className='Labels'>Body</label>
-        <input type='text' className='Body-Textbox' placeholder='Ask your question here' />
+        <input type='text' className='Body-Textbox' placeholder='Ask your question here' value={question}/>
         <label className='Labels'>Tags</label>
-        <select name='Tags' className='Labels'>
-            <option value={"Tag1"}>Tag1</option>
-            <option value={"Tag2"}>Tag2</option>
-            <option value={"Tag3"}>Tag3</option>
-            <option value={"Tag4"}>Tag4</option>
+        <select name='Tags' className='Select-Choice'>
+            <option value={tag}>Tag1</option>
+            <option value={tag}>Tag2</option>
+            <option value={tag}>Tag3</option>
+            <option value={tag}>Tag4</option>
         </select>
         {/* <input type='select' className='Tags-Textbox'/> */}
         <button className='button'>Post Your Question</button>
